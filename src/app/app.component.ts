35a6c5b7d3d5c0dfc86a1d6b1a79cc7f6f172fd1
Jethva, Mahesh <mjethvar8@gmail.com>
2018-01-17 11:59:08 -0500

    second commit
    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+// src/app/app.component.ts
+
+// import animation-related functions
+import { Component, trigger, state, style, animate, transition } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // animations metadata, we define a trigger with two possible states
+  animations: [
+    trigger('movable', [
+      state('fixed', style({
+
+      })),
+      state('roaming', style({
+        'background-color': 'green',
+        'left': '90%'
+      })),
+      // define a transition from one state to another, and an associated animation  
+      transition('* => *', animate('5s 0s ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Stock Dashboard';
+  moving: string;
+  moveIt() {
+    console.log('on the move');
+    // change the state of moving to roaming, activating the trigger and animation    
+    this.moving = 'roaming';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
+// src/app.module.ts
+
+// import Angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// a local module, our main component
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
